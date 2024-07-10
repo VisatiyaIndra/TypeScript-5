599969b89e8f084884e5f6a98e384a14003b58ac
Indra Visatiya <visatiyaindra@gmail.com>
2024-07-11 06:27:16 +1200

    Added lodash

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ import { Movie, Logger, CastMember as Actor } from "./interfaces";
 import { Performer, Documentary, Favorites } from "./classes";
 import * as Utility from "./functions";
 import { runGeneric } from "./generics";
-
+import * as _ from "lodash";
 
 let allMovies: Array<Movie> = Utility.GetAllMovies();
+
+allMovies.forEach(movie => console.log(_.snakeCase(movie.title)));
